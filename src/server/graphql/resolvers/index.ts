282c22c5a6a3552ee907897cf7938da44980c813
Guido Ferraro <gdoferraro@gmail.com>
2022-04-12 18:14:47 -0300

    Nexus placeholder exports

--- src/server/graphql/resolvers/index.ts
+++ src/server/graphql/resolvers/index.ts
@@ -2,3 +2,6 @@ export * from "./Item";
 export * from "./User";
 export * from "./Project";
 export * from "./Brand";
+export * from "./commons";
+export * from "./Retail";
+export * from "./PreUserBrand";
