2869074e9e5d9e89a51c7d4efc12de88d16b050c
Guido Ferraro <gdoferraro@gmail.com>
2022-04-13 19:16:25 -0300

    createPreUserBrand and graphQL schema update through generate script

--- src/server/graphql/resolvers/PreUserBrand/mutations.ts
+++ src/server/graphql/resolvers/PreUserBrand/mutations.ts
@@ -1,6 +1,19 @@
 import { mutationField, nonNull, nullable } from "nexus";
 import { PreUserBrand } from "./models";
 import { WhereUniqueId } from "../commons";
+import { CreatePreUserBrandInput } from "./inputs";
+
+export const createPreUserBrand = mutationField("createPreUserBrand", {
+	type: nullable(PreUserBrand),
+	args: { input: nonNull(CreatePreUserBrandInput) },
+	resolve: async (root, args, ctx) => {
+		return ctx.prisma.preUserBrand.create({
+			data: {
+				...args.input,
+			},
+		});
+	},
+});
 
 export const removePreUserBrand = mutationField("removePreUserBrand", {
 	type: nullable(PreUserBrand),
