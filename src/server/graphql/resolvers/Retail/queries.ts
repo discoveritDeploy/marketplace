b85ac32c0fa9bab70839c39e9bc576122f941fe2
Guido Ferraro <gdoferraro@gmail.com>
2022-04-13 14:45:06 -0300

    Query retail by id and query all

--- src/server/graphql/resolvers/Retail/queries.ts
+++ src/server/graphql/resolvers/Retail/queries.ts
@@ -1,1 +1,24 @@
-export {};
+import { list, nonNull, nullable, queryField } from "nexus";
+import { Retail } from "./models";
+import { WhereUniqueId } from "../commons/inputs";
+
+export const retail = queryField("retail", {
+	type: nullable(Retail),
+	args: {
+		where: nonNull(WhereUniqueId),
+	},
+	resolve: async (root, args, ctx) => {
+		return ctx.prisma.retail.findUnique({
+			where: {
+				...args.where,
+			},
+		});
+	},
+});
+
+export const retails = queryField("retails", {
+	type: nullable(list(nonNull(Retail))),
+	resolve: async (root, args, ctx) => {
+		return ctx.prisma.retail.findMany({});
+	},
+});
