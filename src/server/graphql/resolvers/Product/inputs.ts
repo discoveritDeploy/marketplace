d35ad2c0d257be08b5fc91add007a122ba3185ae
Guido Ferraro <gdoferraro@gmail.com>
2022-04-13 19:23:50 -0300

    createProduct and graphQL schema update through generate script

--- src/server/graphql/resolvers/Product/inputs.ts
+++ src/server/graphql/resolvers/Product/inputs.ts
@@ -1,1 +1,45 @@
-export {};
+import { inputObjectType } from "nexus";
+
+export const ReferencePriceInput = inputObjectType({
+	name: "ReferencePriceInput",
+	definition(t) {
+		t.float("avgSellingPrice");
+		t.float("markUp");
+	},
+});
+
+export const PriceInput = inputObjectType({
+	name: "PriceInput",
+	definition(t) {
+		t.float("originalPrice");
+		t.field("referencePrice", { type: ReferencePriceInput }); // todo chequear tipo generado de un campo opcional nullable
+	},
+});
+
+export const ClaimsPolicyInput = inputObjectType({
+	name: "ClaimsPolicyInput",
+	definition(t) {
+		t.boolean("acceptsRefund");
+	},
+});
+
+export const TechnicalSpecificationInput = inputObjectType({
+	name: "TechnicalSpecificationInput",
+	definition(t) {
+		t.string("description");
+	},
+});
+
+export const CreateProductInput = inputObjectType({
+	name: "CreateOrderInput",
+	definition(t) {
+		t.id("id");
+		t.string("title");
+		t.string("titleDescription");
+		t.float("minQSale");
+		t.field("price", { type: PriceInput });
+		t.field("claimsPolicy", { type: ClaimsPolicyInput });
+		t.field("technicalSpecification", { type: TechnicalSpecificationInput });
+		t.int("selledUnits");
+	},
+});
--- src/server/graphql/resolvers/Product/mutations.ts
+++ src/server/graphql/resolvers/Product/mutations.ts
@@ -13,3 +13,17 @@ export const removeProduct = mutationField("removeProduct", {
 		});
 	},
 });
+
+import { CreateProductInput } from "./inputs";
+
+export const createProduct = mutationField("createProduct", {
+	type: nullable(Product),
+	args: { input: nonNull(CreateProductInput) },
+	resolve: async (root, args, ctx) => {
+		return ctx.prisma.product.create({
+			data: {
+				...args.input,
+			},
+		});
+	},
+});
