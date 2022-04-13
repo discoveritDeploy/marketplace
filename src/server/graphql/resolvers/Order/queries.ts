3d56a8e7207306e79a07bd86058faf524d40b21c
Guido Ferraro <gdoferraro@gmail.com>
2022-04-13 14:42:19 -0300

    Query order by id and query all

--- src/server/graphql/resolvers/Order/queries.ts
+++ src/server/graphql/resolvers/Order/queries.ts
@@ -1,1 +1,24 @@
-export {};
+import { list, nonNull, nullable, queryField } from "nexus";
+import { Order } from "./models";
+import { WhereUniqueId } from "../commons/inputs";
+
+export const order = queryField("order", {
+	type: nullable(Order),
+	args: {
+		where: nonNull(WhereUniqueId),
+	},
+	resolve: async (root, args, ctx) => {
+		return ctx.prisma.order.findUnique({
+			where: {
+				...args.where,
+			},
+		});
+	},
+});
+
+export const orders = queryField("orders", {
+	type: nullable(list(nonNull(Order))),
+	resolve: async (root, args, ctx) => {
+		return ctx.prisma.order.findMany({});
+	},
+});
